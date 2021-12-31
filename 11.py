e963787f5828bb6d0da7147ed9c5145705f7c51c
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-01 03:19:44 +0800

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,11 +1,10 @@
 for i in range(2, 21):
 	for j in range(2, i):
 		if i % j == 0:
-			print(i, " = ", j, " times ", i // j)
+			print(f"{i} = {j} times {i // j}")
 			break
 				    
 	else:
 		print(i, " is a prime number!")
-			
-			
-print("done")
+
+print("Done")
