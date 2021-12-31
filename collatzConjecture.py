e4cbecef76c08da62fa1ce9de045bc1c303eb089
James Tang <70836155+yonghuatang@users.noreply.github.com>
2021-12-31 19:27:52 +0800

    Update and rename Collatz Conjecture.py to collatzConjecture.py

---
+++ collatzConjecture.py
@@ -0,0 +1,13 @@
+x = int(input("Please enter a positive integer: "))
+
+steps = 0
+
+while x > 1:
+    steps += 1
+    if x % 2 == 0:
+        x /= 2
+    else:
+        x = 3*x + 1
+    print(x)
+  
+print(steps)
