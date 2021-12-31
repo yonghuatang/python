271bbd1d20559cef1c4839b6c9da3b34ff1b2f9f
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-01 03:24:36 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,24 +1,5 @@
-print("""
-Hi!
-How are you today?
-""")
-
 from datetime import date
 now = date.today()
 print('The date today is', now, now.strftime("%A"))
 
-for x in range(1, 101):
-    if x%3 == 0:
-        print("OH IT'S FACTOR OF 3!")
-        pass
-    
-    else:
-        print(x)
-
 print("DONE")
-
-        
-
-
-
-
