2bf4f7c1d63ff07587a0f9fd2b168159f4f1cd5d
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-01 03:26:30 +0800

    Update and rename 20.py to big_num.py

--- big_num.py
+++ big_num.py
@@ -5,8 +5,8 @@ big_num = 0
 while True:
     n = int(input("Please enter a number (type 0 to exit): "))
     if n == 0:
+        print("Program terminated. . .")
         break
     if n > big_num:
         big_num = n
-
-print(big_num)
+        print(big_num)
