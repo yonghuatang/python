0ea473e5d5a2d3d4d96e9bcef0c2e6b08351947e
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-02 20:01:40 +0800

    Update print_format.py

--- print_format.py
+++ print_format.py
@@ -1,6 +1,8 @@
-# Created by YongHua
-
 first_name = "John"
 last_name = "Doe"
 
+# First way
 print("My name is {} {}!".format(first_name, last_name))
+
+# Second way
+print(f"My name is {first_name} {last_name}!")
