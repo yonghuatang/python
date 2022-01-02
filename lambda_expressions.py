4e270924356dff7c89fa60d46023630490490339
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-02 20:34:13 +0800

    Update lambda_expressions.py

--- lambda_expressions.py
+++ lambda_expressions.py
@@ -1,19 +1,23 @@
 ## lambda expressions
-## Created by Yonghua 20 Jul 2020
+## Created by Yonghua - 20 Jul 2020
 
 def f(x):
     return 3*x - 1
 
-print(f(5))
 
-# or using lambda expressions,
+# or similarly using lambda expressions
 
-f = lambda x: 3*x - 1
-print(f(5))
+g = lambda x: 3*x - 1
+
 
 ### example (quadratic equations)
 
-def quad(a, b, c):
+def createQuadratic(a, b, c):
     return lambda x: a*x**2 + b*x + c
-result = quad(4, 9, 31)
-print(result(35))
+
+
+if __name__ == "__main__":
+    print(f(5))
+    print(g(5))
+    h = createQuadratic(4, 9, 31)  # creates 4*x**2 + 9*x + 31
+    print(h(11))
