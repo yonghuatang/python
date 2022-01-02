75c4c4524e02d73b7863e38e51200d575faf9ccb
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-01-02 20:05:58 +0800

    Rename tkinter2.py to tkinter3.py

--- tkinter3.py
+++ tkinter3.py
@@ -1,6 +1,8 @@
 # Similar to prior, but use classes so each window has own state information
+
 from tkinter import *
 import random
+
 class MyGui:
     """
     A GUI with buttons that change color and make the label grow
@@ -15,26 +17,34 @@ class MyGui:
         Button(parent, text='Spam', command=self.reply).pack(side=LEFT)
         Button(parent, text='Grow', command=self.grow).pack(side=LEFT)
         Button(parent, text='Stop', command=self.stop).pack(side=LEFT)
+        
     def reply(self):
         "change the button's color at random on Spam presses"
         self.fontsize += 5
         color = random.choice(self.colors)
         self.lab.config(bg=color,
         font=('courier', self.fontsize, 'bold italic'))
+        
     def grow(self):
         "start making the label grow on Grow presses"
         self.growing = True
         self.grower()
+        
     def grower(self):
         if self.growing:
             self.fontsize += 5
             self.lab.config(font=('courier', self.fontsize, 'bold'))
             self.lab.after(500, self.grower)
+            
     def stop(self):
         "stop the button growing on Stop presses"
         self.growing = False
+        
+        
 class MySubGui(MyGui):
     colors = ['black', 'purple'] # Customize to change color choices
+
+
 MyGui(Tk(), 'main')
 MyGui(Toplevel())
 MySubGui(Toplevel())
